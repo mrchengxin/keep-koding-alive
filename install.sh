26a58b2dda4698b0b3b85bb22df8f8565bd9218d
Aaron <mrchengxin@gmail.com>
2015-12-12 23:28:43 +0800

    add phantomjs

--- install.sh
+++ install.sh
@@ -16,7 +16,6 @@ sudo pip install supervisor
 sudo cp ./shadowsocks.conf /etc/supervisor/conf.d/shadowsocks.conf
 sudo supervisorctl update
 sudo mkdir /home/keepOn
-sudo git clone https://github.com/Pyppe/phantomjs2.0-ubuntu14.04x64.git
-sudo cp phantomjs2.0-ubuntu14.04x64/bin/phantomjs /home/keepOn/phantomjs
+sudo cp ./phantomjs /home/keepOn/phantomjs
 sudo cp ./koding.js /home/keepOn/koding.js
 crontab -l | { cat; echo "*/10 0-14 * * * /home/keepOn/phantomjs /home/keepOn/koding.js $username $password > /home/keepOn/log"; } | crontab -
