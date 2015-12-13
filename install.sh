e014f6bc36a2d1fc234da9dbcda512fa58acb64c
Aaron <mrchengxin@gmail.com>
2015-12-13 11:51:27 +0800

    adjust directory structure

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ sudo apt-get update
 sudo apt-get install -y python-pip
 sudo pip install shadowsocks
 sudo mkdir /opt/shadowsocks
-sudo cp ./config.json /opt/shadowsocks/config.json
-sudo bash ./serverSpeederInstaller.sh -e $serverSpeederEmail -p $serverSpeederPassword -in 1000000 -out 1000000 -i eth0 -r -t 0 -gso 1 -rsc 1 -b -f
-sudo cp serverSpeeder.conf /serverspeeder/etc/config
+sudo cp ./shadowsocks/config.json /opt/shadowsocks/config.json
+sudo bash ./serverspeeder/serverSpeederInstaller.sh -e $serverSpeederEmail -p $serverSpeederPassword -in 1000000 -out 1000000 -i eth0 -r -t 0 -gso 1 -rsc 1 -b -f
+sudo cp ./serverspeeder/serverSpeeder.conf /serverspeeder/etc/config
 sudo service serverSpeeder restart
 sudo apt-get install -y supervisor
-sudo cp ./shadowsocks.conf /etc/supervisor/conf.d/shadowsocks.conf
+sudo cp ./supervisor/shadowsocks.conf /etc/supervisor/conf.d/shadowsocks.conf
 sudo supervisorctl update
 sudo mkdir /home/keepOn
-sudo cp ./phantomjs /home/keepOn/phantomjs
-sudo cp ./koding.js /home/keepOn/koding.js
+sudo cp ./phantomjs/phantomjs /home/keepOn/phantomjs
+sudo cp ./phantomjs/koding.js /home/keepOn/koding.js
 crontab -l | { cat; echo "*/10 0-14 * * * /home/keepOn/phantomjs /home/keepOn/koding.js $username $password > /home/keepOn/log"; } | crontab -
